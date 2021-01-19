255c323418ce98896977594b4771d0d2c7faab1c
howToNity <68746727+howToNity@users.noreply.github.com>
2021-01-19 11:54:13 -0500

    Simple URL Shrinker

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const ShortUrl = require("./models/shortUrl");
+const app = express();
+
+mongoose.connect("mongodb://localhost:27017/URL-Shortener", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", async (req, res) => {
+  const shortUrls = await ShortUrl.find();
+  res.render("index", { shortUrls: shortUrls });
+});
+
+app.post("/shortUrls", async (req, res) => {
+  await ShortUrl.create({
+    full: req.body.fullUrl,
+  });
+  res.redirect("/");
+});
+
+app.get("/r/:shortUrl", async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
+  if (shortUrl == null) res.sendStatus(404);
+
+  shortUrl.clicks++;
+  shortUrl.save();
+
+  res.redirect(shortUrl.full);
+});
+
+app.listen(process.env.PORT || 5000, () =>
+  console.log("Server up and running")
+);
